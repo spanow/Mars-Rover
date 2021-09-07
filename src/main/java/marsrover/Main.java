7926da8f353e2755c9836e2a82618f7b0c13cdb1
spanow <77399761+spanow@users.noreply.github.com>
2021-09-07 10:40:53 +0200

    Passage vers ressources

--- src/main/java/marsrover/Main.java
+++ src/main/java/marsrover/Main.java
@@ -13,15 +13,15 @@ class Main {
 
     public static void main(String[] args) throws IOException {
 
-        ArrayList<String> inputFileAsList = InputUtil.parseInputFromFile(Main.class.getResourceAsStream(args[0]));
+        ArrayList<String> inputFileAsList = InputUtil.parseInputFromFile(Main.class.getResourceAsStream("/input.txt"));
         // The first instruction is separated out, as this is for creating the Plateau, which is global for all Rovers
         List<String> roverInput = inputFileAsList.subList(1, inputFileAsList.size());
 
-        System.out.println("Input:" + "\n");
+        System.out.println("Fichier d'entrée:" + "\n");
         for (String line : inputFileAsList)
             System.out.println(line);
 
-        System.out.println("\n" + "Output:" + "\n");
+        System.out.println("\n" + "Sortie:" + "\n");
         Plateau plateau = InputUtil.parsePlateauInput(inputFileAsList.get(0));
 
 
